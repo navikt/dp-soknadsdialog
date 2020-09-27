d16e249a9d58e093361ea44dcf4fd56c4d993484
André Roaldseth <andre.roaldseth@nav.no>
2020-09-27 20:40:49 +0200

    Proxy med pipe

---
+++ pages/api/_proxy.js
@@ -0,0 +1,33 @@
+import { request } from "http";
+
+export default function proxy(url = new URL(""), req, res) {
+  return new Promise((resolve) => {
+    const proxy = request(
+      url,
+      {
+        method: req.method,
+        headers: req.headers,
+      },
+      (resp) => {
+        res.statusCode = resp.statusCode;
+        copyHeaders(resp.headers, res);
+
+        resp.pipe(res);
+
+        resp.on("end", () => {
+          res.end();
+          resolve(resp);
+        });
+      }
+    );
+
+    req.pipe(proxy);
+  });
+}
+
+const copyHeaders = (headers, res) => {
+  const bannedHeaders = ["server"];
+  Object.entries(headers)
+    .filter(([header]) => !!bannedHeaders.indexOf(header))
+    .forEach(([header, value]) => res.setHeader(header, value));
+};
--- pages/api/soknad/[id]/neste-seksjon.js
+++ pages/api/soknad/[id]/neste-seksjon.js
@@ -1,10 +1,9 @@
+import proxy from "../../_proxy";
+
 export default async (req, res) => {
   const {
     query: { id },
   } = req;
-  const fakta = await fetch(`http://dp-quiz-api/soknad/${id}/neste-seksjon`);
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(await fakta.text());
+  const url = new URL(`http://localhost:8080/soknad/${id}/neste-seksjon`);
+  await proxy(url, req, res);
 };
