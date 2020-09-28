9a907ea8f2db8b27677baf293c483872d36687d4
André Roaldseth <andre.roaldseth@nav.no>
2020-09-28 12:26:01 +0200

    Fix URL i neste-seksjon

--- pages/api/soknad/[id]/neste-seksjon.js
+++ pages/api/soknad/[id]/neste-seksjon.js
@@ -4,6 +4,6 @@ export default async (req, res) => {
   const {
     query: { id },
   } = req;
-  const url = new URL(`http://localhost:8080/soknad/${id}/neste-seksjon`);
+  const url = new URL(`http://dp-quiz-api/soknad/${id}/neste-seksjon`);
   await proxy(url, req, res);
 };
