9ec6400709d53e60aee7283767aba294165432d6
André Roaldseth <andre.roaldseth@nav.no>
2020-09-30 10:34:41 +0200

    Import lodash-moduler direkte

--- components/spørsmål.js
+++ components/spørsmål.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { debounce } from "lodash";
+import debounce from "lodash/debounce";
 import InputSpørsmål from "./input-spørsmål";
 import DatoSpørsmål from "./dato-spørsmål";
 
