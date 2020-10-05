5d38676a8df9f513217bad303fb34acaadab90a1
Andreas Nygård Bergh <andreas.nygard.bergh@nav.no>
2020-10-05 12:04:37 +0200

    La subsumsjon rendre sine egne data, og ikke sine barns data
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: kriise <kmriise@gmail.com>

--- components/subsumsjon.js
+++ components/subsumsjon.js
@@ -1,41 +1,39 @@
-export default function Subsumsjon({ subsumsjoner = [], gyldig, dybde = 0 }) {
+export default function Subsumsjon({
+  navn,
+  subsumsjoner = [],
+  resultat = false,
+  fakta = [],
+  gyldig,
+  dybde = 0,
+}) {
   return (
-    <div>
-      {subsumsjoner.map(
-        ({ navn, resultat = null, fakta = [], subsumsjoner, gyldig }) => (
-          <div
-            key={navn}
-            data-testid="subsumsjon"
-            style={{ paddingLeft: dybde * 15 }}
-          >
-            {resultat === true && "✅"}
-            {resultat === false && "❌"}
-            {resultat == null && "❓"}
-            &nbsp;&nbsp;
-            {navn}
-            {!!fakta.length && (
-              <ul>
-                {fakta.map((faktum) => (
-                  <li key={faktum.id} data-testid="faktum">
-                    {faktum.svar !== undefined ? "✅" : "❓"}
-                    &nbsp;&nbsp;
-                    {faktum.navn}
-                    <br />
-                    <b>Svar:</b> {faktum.svar}
-                    {faktum.svar === true && "✅"}
-                    {faktum.svar === false && "❌"}
-                  </li>
-                ))}
-              </ul>
-            )}
-            <Subsumsjon
-              subsumsjoner={subsumsjoner}
-              dybde={dybde + 1}
-              gyldig={gyldig}
-            />
-          </div>
-        )
+    <div
+      key={navn}
+      data-testid="subsumsjon"
+      style={{ paddingLeft: dybde * 15 }}
+    >
+      {resultat && "✅"}
+      {!resultat && "❌"}
+      &nbsp;&nbsp;
+      {navn}
+      {!!fakta.length && (
+        <ul>
+          {fakta.map((faktum) => (
+            <li key={faktum.id} data-testid="faktum">
+              {faktum.svar !== undefined ? "✅" : "❓"}
+              &nbsp;&nbsp;
+              {faktum.navn}
+              <br />
+              <b>Svar:</b> {faktum.svar}
+              {faktum.svar === true && "✅"}
+              {faktum.svar === false && "❌"}
+            </li>
+          ))}
+        </ul>
       )}
+      {subsumsjoner.map((subsumsjon) => (
+        <Subsumsjon {...subsumsjon} dybde={dybde + 1} gyldig={gyldig} />
+      ))}
       {gyldig && <Subsumsjon {...gyldig}></Subsumsjon>}
     </div>
   );
