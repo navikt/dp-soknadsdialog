18a8960bcfce6b59e10f4a9526cbd68050167984
Geir André Lund <geir.andre.lund@nav.no>
2021-11-02 19:23:05 +0100

    Litt fikling med .dockerignore for å få det til å bygge i Docker. Mulig vi burde restruktere prosjektet Bruke swr hook for å hente data fra api
    navikt/dagpenger#959

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -1,26 +1,17 @@
 import Head from "next/head";
 import Link from "next/link";
-import { Home as HomeIcon } from "@navikt/ds-icons";
-import { LinkPanel, Heading, Ingress } from "@navikt/ds-react";
+import {Home as HomeIcon} from "@navikt/ds-icons";
+import {Heading, Ingress, LinkPanel} from "@navikt/ds-react";
 import Subsumsjoner from "../components/subsumsjoner";
 import Søknad from "../containers/søknad";
-
+import useSwr from 'swr'
+const fetcher = (url) => fetch(url, {method: 'POST'}).then((res) => res.json())
 
 export default function Quiz() {
+  const { data, error } = useSwr(`${process.env.NEXT_PUBLIC_API_URL}/soknad`, fetcher)
 
-  const nySøknad = async () => {
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/soknad`,   {
-          method: "POST"
-        }
-    );
-
-    if (response.status === 201) {
-      return await response.json();
-    }
-  };
-
-  const søknadsId = nySøknad()["uuid"]
+  if (!data) return <div>Laster...</div>
+  if (error) return <div>Kunne ikke opprette søknad</div>
 
   return (
     <div className="root">
@@ -31,36 +22,16 @@ export default function Quiz() {
       <section>
         <div className="fo">
           <HomeIcon style={{ height: "20px" }} />
-          <Heading level="1" size="2xlarge">Hola!</Heading>
-          <Ingress spacing>Nå tester vi quiz</Ingress>
+          <Heading level="1" size="2xlarge">Søknad om dagpenger</Heading>
         </div>
       </section>
 
       <main>
-        <div className="boxes">
-          <LinkPanel className="half" href="/" border>
-            <LinkPanel.Title>
-              Koronavirus - hva gjelder i min situasjon?
-            </LinkPanel.Title>
-            <LinkPanel.Description>
-              Se informasjon om rettighetene dine
-            </LinkPanel.Description>
-          </LinkPanel>
-          <LinkPanel className="half" href="/" border>
-            <LinkPanel.Title>
-              Koronavirus - hva gjelder i min situasjon?
-            </LinkPanel.Title>
-            <LinkPanel.Description>
-              Se informasjon om rettighetene dine
-            </LinkPanel.Description>
-          </LinkPanel>
-        </div>
 
-        <Søknad id={søknadsId} />
-        <Subsumsjoner søknadId={søknadsId} />
+        <Søknad id={data.uuid} />
+        <Subsumsjoner søknadId={data.uuid} />
 
-        <p className="typo-normal">PartyTime</p>
-        <Link href="/">Ta en tur hjem igjen</Link>
+        <Link href="/">Tilbake</Link>
       </main>
     </div>
   );
