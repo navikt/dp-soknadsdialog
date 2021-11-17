ce5359747844252bf6bcce874fcbc15300fd0ec9
Geir André Lund <geir.andre.lund@nav.no>
2021-11-17 13:16:41 +0100

    Put gjennom proxy fungerer ikke

--- src/pages/api/soknad/[id]/faktum/[faktumId].ts
+++ src/pages/api/soknad/[id]/faktum/[faktumId].ts
@@ -1,4 +1,3 @@
-import proxy from "../../../_proxy";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const faktumLagreHandler = async (
@@ -9,10 +8,18 @@ const faktumLagreHandler = async (
     query: { id, faktumId },
   } = req;
 
-  const url = new URL(
-    `${process.env.API_BASE_URL}/soknad/${id}/faktum/${faktumId}`
+  const fakta = await fetch(
+    `${process.env.API_BASE_URL}/soknad/${id}/faktum/${faktumId}`,
+    {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: req.body,
+    }
   );
-  await proxy(url, req, res);
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(await fakta.text());
 };
 
 export default faktumLagreHandler;
