65db75437c08e4b1884abe4d301dd3d45be1d4a6
Geir André Lund <geir.andre.lund@nav.no>
2021-11-18 10:00:23 +0100

    Lite hack - vi har bare støtte for boolean faktum

--- src/components/seksjon.tsx
+++ src/components/seksjon.tsx
@@ -1,25 +1,37 @@
 import { Quiz } from "../models/quiz";
 import { RadioButtonInput } from "./inputs/radio-input.component";
 import { lagreFaktum } from "../services/api";
+import { TextField } from "@navikt/ds-react";
 
-function Seksjon({ id, seksjon }: { id: any; seksjon: Quiz.Seksjon }) {
+function Seksjon({ seksjon }: { seksjon: Quiz.Seksjon }) {
   return (
     <>
       Seksjonsnavn: {seksjon.seksjon_navn}
       {seksjon &&
-        seksjon.fakta.map((faktum) => (
-          <RadioButtonInput
-            key={faktum.id}
-            legend={faktum.navn}
-            options={[
-              { text: "Ja", value: "true" },
-              { text: "Nei", value: "false" },
-            ]}
-            onSelection={(verdi) =>
-              lagreFaktum(id, faktum.id, faktum.type, verdi)
-            }
-          />
-        ))}
+        seksjon.fakta
+          .filter((f) => f.type === Quiz.DataType.BOOLEAN)
+          .map(({ id, navn, type }) => (
+            <RadioButtonInput
+              key={id}
+              legend={navn}
+              options={[
+                { text: "Ja", value: "true" },
+                { text: "Nei", value: "false" },
+              ]}
+              onSelection={(verdi) => lagreFaktum(id, id, type, verdi)}
+            />
+          ))}
+      {seksjon &&
+        seksjon.fakta
+          .filter((f) => f.type !== Quiz.DataType.BOOLEAN)
+          .map(({ id, navn, type }) => (
+            <TextField
+              key={id}
+              label={`${navn} (ikke støttet :-( )`}
+              type={"text"}
+              data-testid={`input-${id}`}
+            />
+          ))}
     </>
   );
 }
