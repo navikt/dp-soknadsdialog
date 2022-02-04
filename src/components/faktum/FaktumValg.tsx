5c7bee404e16c4d342b7c8ce5c639b508532214f
Sindre Sægrov <sindre.segrov@nav.no>
2022-02-04 09:03:28 +0100

    Add alert for helptext and alerttext

--- src/components/faktum/FaktumValg.tsx
+++ src/components/faktum/FaktumValg.tsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Radio, RadioGroup } from "@navikt/ds-react";
+import { Alert, Radio, RadioGroup } from "@navikt/ds-react";
 import { Faktum } from "./Faktum";
 import { IValgFaktum } from "../../types/faktum.types";
 import styles from "./Faktum.module.css";
 
 export function FaktumValg(props: IValgFaktum) {
-  const [answer, setAnswer] = useState("");
+  const [answerState, setAnswerState] = useState("");
 
   useEffect(() => {
     // eslint-disable-next-line no-console
-    console.log("Ny verdi: ", answer);
-  }, [answer]);
+    console.log("Ny verdi: ", answerState);
+  }, [answerState]);
 
   function onChange(value: string) {
-    setAnswer(value);
+    setAnswerState(value);
   }
 
   return (
@@ -24,16 +24,22 @@ export function FaktumValg(props: IValgFaktum) {
 
       <RadioGroup legend={props.title ? props.title : props.id} onChange={onChange}>
         {props.answerOptions.map((answer) => (
-          <Radio key={answer.id} value={answer.id}>
-            {answer.title ? answer.title : answer.id}
-          </Radio>
+          <>
+            <Radio key={answer.id} value={answer.id}>
+              {answer.title ? answer.title : answer.id}
+            </Radio>
+            {answer.helpText ? <Alert variant={"info"}>{answer.helpText}</Alert> : undefined}
+            {answer.alertText && answerState === answer.id ? (
+              <Alert variant={"warning"}>{answer.alertText}</Alert>
+            ) : undefined}
+          </>
         ))}
       </RadioGroup>
 
       {props.subFaktum && props.subFaktum.length > 0 && (
         <div className={styles["sub-faktum"]}>
           {props.subFaktum.map((faktum) => {
-            if (faktum.requiredAnswerIds.find((a) => a.id === answer)) {
+            if (faktum.requiredAnswerIds.find((a) => a.id === answerState)) {
               return <Faktum key={faktum.id} {...faktum} />;
             }
           })}
