9ac518dfa1025d85aef435eb01a0ba16e74c43c9
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-02-07 12:19:54 +0100

    Fjernet fakta-kall DAG-123

--- src/pages/api/soknad/index.ts
+++ src/pages/api/soknad/index.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { sanityClient } from "../../../../sanity-client";
-import api, { host } from "../../../api.utils";
 import { fetchAllSeksjoner } from "../../../sanity/groq-queries";
 import { ISeksjon } from "../../../types/seksjon.types";
 
@@ -10,12 +9,6 @@ export interface ISoknad {
 
 const soknad = async (req: NextApiRequest, res: NextApiResponse) => {
   const sanitySections = await sanityClient.fetch<ISeksjon[]>(fetchAllSeksjoner);
-
-  const fakta = await fetch(new URL(`${api("soknad/123/fakta")}`, host).href).then((data) => {
-    return data.json();
-  });
-  // eslint-disable-next-line no-console
-  console.log(fakta);
 
   if (sanitySections.length <= 0) {
     // eslint-disable-next-line no-console
