8608ca72256a2274aa8a4c5706ae9c8c2e5c3922
Sindre Sægrov <sindre.segrov@nav.no>
2022-02-10 13:59:51 +0100

    Fix type

--- src/types/faktum.types.ts
+++ src/types/faktum.types.ts
@@ -1,5 +1,5 @@
 import { TypedObject } from "@portabletext/types";
-export type ValgFaktumType = "boolean" | "valg" | "dropdown" | "flervalg";
+export type ValgFaktumType = "boolean" | "valg" | "dropdown" | "flervalg" | "land";
 export type PrimitivFaktumType = "int" | "double" | "localdate" | "periode" | "tekst";
 export type GeneratorFaktumType = "generator";
 export type GeneratorListType = "Arbeidsforhold" | "Barn" | "Standard";
