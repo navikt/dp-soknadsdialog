a07b713814dcf970ab0d78133f3bc88c91d15f31
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-02-11 11:20:43 +0100

    Fetching sections if not present on state

--- src/pages/[soknadId]/seksjon/[id].tsx
+++ src/pages/[soknadId]/seksjon/[id].tsx
@@ -1,8 +1,21 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Seksjon } from "../../../components/seksjon/Seksjon";
 import { RootState } from "../../../store";
+import { setSeksjoner } from "../../../store/seksjoner.slice";
+import { ISoknad } from "../../api/soknad";
 export default function SeksjonPage() {
   const sections = useSelector((state: RootState) => state.seksjoner);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!sections.length) {
+      fetch("/api/soknad")
+        .then((response: Response) => response.json())
+        .then((data: ISoknad) => {
+          dispatch(setSeksjoner(data.sections));
+        });
+    }
+  }, [sections]);
   return sections.map((section) => <Seksjon key={section.id} {...section} />);
 }
