b07ad802fc918214c6f706b28c306285e33e7f47
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-02-22 13:52:49 +0100

    Fixed mapping issue

--- src/pages/[uuid]/index.tsx
+++ src/pages/[uuid]/index.tsx
@@ -3,19 +3,55 @@ import { Provider } from "react-redux";
 import { initialiseStore, RootState } from "../../store";
 import { Soknad } from "../../components/view/Soknad";
 import { GetServerSidePropsContext, GetServerSidePropsResult } from "next/types";
+import { sanityClient } from "../../../sanity-client";
+import { fetchAllSeksjoner } from "../../sanity/groq-queries";
+import { ISection } from "../../types/section.types";
+import { getSession } from "@navikt/dp-auth/server";
+import { audience } from "../../api.utils";
+import { getFakta } from "../../server-side/quiz-api";
+import { mapQuizFaktaToReduxState } from "../../server-side/quiz-to-redux-mapper";
 
 export async function getServerSideProps(
   context: GetServerSidePropsContext
 ): Promise<GetServerSidePropsResult<RootState>> {
   const { query } = context;
-  const uuid = query.uuid;
-  const response: Response = await fetch(
-    `${process.env.SELF_URL}/api/soknad/${uuid}/get-initial-redux-state`
-  );
-  const initialReduxState: RootState = await response.json();
+  const uuid = query.uuid as string;
+
+  const sanitySections = await sanityClient.fetch<ISection[]>(fetchAllSeksjoner);
+
+  if (sanitySections.length <= 0) {
+    // eslint-disable-next-line no-console
+    console.error("Fant ikke seksjon i sanity");
+    return { notFound: true };
+  }
+
+  const { token, apiToken } = await getSession(context);
+  let initialState: RootState = {
+    soknadId: uuid,
+    sections: sanitySections,
+    answers: [],
+    barnetillegg: { id: "", beskrivendeId: "faktum.barn-liste", type: "generator", answers: [] },
+    arbeidsforhold: {
+      id: "",
+      beskrivendeId: "faktum.arbeidsforhold",
+      type: "generator",
+      answers: [],
+    },
+  };
+
+  if (process.env.NEXT_PUBLIC_LOCALHOST) {
+    const fakta = await getFakta(uuid, "");
+    initialState = { ...initialState, ...mapQuizFaktaToReduxState(fakta) };
+  }
+
+  if (token && apiToken) {
+    const onBehalfOfToken = await apiToken(audience);
+    const fakta = await getFakta(uuid, onBehalfOfToken);
+    initialState = { ...initialState, ...mapQuizFaktaToReduxState(fakta) };
+  }
 
   return {
-    props: { ...initialReduxState },
+    props: { ...initialState },
   };
 }
 
