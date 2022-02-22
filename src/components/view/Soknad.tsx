c1924e66300f5edf2f1ae17bc38e3c1b0ea149ac
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-02-22 13:38:53 +0100

    Order restored

--- src/components/view/Soknad.tsx
+++ src/components/view/Soknad.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
@@ -7,14 +6,6 @@ import { Seksjon } from "../seksjon/Seksjon";
 
 export function Soknad() {
   const sections = useSelector((state: RootState) => state.sections);
-  const { query } = useRouter();
-  const uuid = query.uuid as string;
-  fetch(`${process.env.SELF_URL}/api/soknad/${uuid}/fakta`).then((data) => {
-    data.json().then((fakta) => {
-      // eslint-disable-next-line no-console
-      console.log(fakta);
-    });
-  });
   return (
     <div>
       {sections.map((section: ISection) => (
