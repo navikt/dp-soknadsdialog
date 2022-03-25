34bd57d16d0c1014ee7d7e2b224f5a7cbe1888ab
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-03-25 13:40:12 +0100

    test file for answer-slice

---
+++ src/store/answer.slice.test.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combinedRootReducer } from ".";
+
+const { getState } = configureStore({
+  reducer: combinedRootReducer,
+});
+
+describe("answerSlice", () => {
+  it("should initialize with empty array", () => {
+    expect(getState().sections).toEqual([]);
+  });
+});
