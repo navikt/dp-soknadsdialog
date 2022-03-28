e7376d7660467c12958e0563b506b00af50913c4
André Roaldseth <andre.roaldseth@nav.no>
2022-03-28 15:10:10 +0200

    Bytt ut dp-quizshow-api med dp-soknad

--- src/api.utils.ts
+++ src/api.utils.ts
@@ -1,5 +1,5 @@
 export const host = process.env.SELF_URL;
-export const audience = `${process.env.NAIS_CLUSTER_NAME}:teamdagpenger:dp-quizshow-api`;
+export const audience = `${process.env.NAIS_CLUSTER_NAME}:teamdagpenger:dp-soknad`;
 
 export default function api(endpoint: string): string {
   return `${process.env.NEXT_PUBLIC_BASE_PATH}/api${
