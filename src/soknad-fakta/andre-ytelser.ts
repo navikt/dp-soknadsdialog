9153e3a079a65448915fffd9818971c84d7d0289
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-03-30 12:45:05 +0200

    Update sanity-keys andre-ytelser

--- src/soknad-fakta/andre-ytelser.ts
+++ src/soknad-fakta/andre-ytelser.ts
@@ -4,76 +4,96 @@ export const andreYtelser: MockDataSeksjon = {
   id: "andre-ytelser",
   faktum: [
     {
-      id: "faktum.andre-ytelser", //todo: de fleste svarene her skal trigge dokumentkrav
-      type: "flervalg",
+      id: "faktum.andre-ytelser-mottatt-eller-sokt",
+      type: "boolean",
       answerOptions: [
-        { id: "faktum.andre-ytelser.svar.pensjon-offentlig-tjenestepensjon" },
-        { id: "faktum.andre-ytelser.svar.arbeidsloshet-garantikassen-for-fiskere" },
-        { id: "faktum.andre-ytelser.svar.garantilott-garantikassen-for-fiskere" },
-        { id: "faktum.andre-ytelser.svar.etterlonn-arbeidsgiver" },
-        { id: "faktum.andre-ytelser.svar.vartpenger" },
-        { id: "faktum.andre-ytelser.svar.dagpenger-annet-eos-land" },
-        { id: "faktum.andre-ytelser.svar.annen-ytelse" },
-        { id: "faktum.andre-ytelser.svar.nei" },
+        { id: "faktum.andre-ytelser-mottatt-eller-sokt.svar.ja" },
+        { id: "faktum.andre-ytelser-mottatt-eller-sokt.svar.nei" },
       ],
       subFaktum: [
         {
-          id: "faktum.tjenestepensjon-hvem-utbetaler-hvilken-periode", //todo: dette bør egentlig være 2 faktum for å kunne automatisere, navn på utbetaler og periode utbetalingen skjer
-          type: "tekst",
-          requiredAnswerIds: ["faktum.andre-ytelser.svar.pensjon-offentlig-tjenestepensjon"],
-        },
-        {
-          id: "faktum.arbeidsloshet-garantikassen-for-fiskere-periode", //todo: dette bør egentlig være periodefaktum for periode utbetalingen skjer
-          type: "tekst",
-          requiredAnswerIds: ["faktum.andre-ytelser.svar.arbeidsloshet-garantikassen-for-fiskere"],
-        },
-        {
-          id: "faktum.garantilott-garantikassen-for-fiskere-periode", //todo: dette bør egentlig være periodefaktum for periode utbetalingen skjer
-          type: "tekst",
-          requiredAnswerIds: ["faktum.andre-ytelser.svar.garantilott-garantikassen-for-fiskere"],
-        },
-        {
-          id: "faktum.etterlonn-hvem-utbetaler-hvilken-periode", //todo: dette bør egentlig være 2 faktum for å kunne automatisere, navn på utbetaler og periode utbetalingen skjer
-          type: "tekst", //todo: kunne dette kanskje vært en dropdown på arbeidsforhold som er lagt inn tidligere i søknaden?
-          requiredAnswerIds: ["faktum.andre-ytelser.svar.etterlonn-arbeidsgiver"],
-        },
-        {
-          id: "faktum.vartpenger-hvem-utbetaler-hvilken-periode", //todo: dette bør egentlig være 2 faktum for å kunne automatisere, navn på utbetaler og periode utbetalingen skjer
-          type: "tekst",
-          requiredAnswerIds: ["faktum.andre-ytelser.svar.vartpenger"],
-        },
-        {
-          id: "faktum.dagpenger-annet-eos-land",
-          type: "land",
-          answerOptions: [], //todo: insert alle verdens land
-          requiredAnswerIds: ["faktum.andre-ytelser.svar.dagpenger-annet-eos-land"],
-        },
-        {
-          id: "faktum.annen-ytelse-hvilken",
-          type: "tekst",
-          requiredAnswerIds: ["faktum.andre-ytelser.svar.annen-ytelse"],
-        },
-        {
-          id: "faktum.annen-ytelse-hvem-utebetaler-hvilken-periode", //todo: dette bør egentlig være 2 faktum for å kunne automatisere, navn på utbetaler og periode utbetalingen skjer
-          type: "tekst",
-          requiredAnswerIds: ["faktum.andre-ytelser.svar.annen-ytelse"],
+          id: "faktum.hvilke-andre-ytelser",
+          type: "flervalg",
+          answerOptions: [
+            { id: "faktum.hvilke-andre-ytelser.svar.pensjon-offentlig-tjenestepensjon" },
+            { id: "faktum.hvilke-andre-ytelser.svar.arbeidsloshet-garantikassen-for-fiskere" },
+            { id: "faktum.hvilke-andre-ytelser.svar.garantilott-garantikassen-for-fiskere" },
+            { id: "faktum.hvilke-andre-ytelser.svar.etterlonn-arbeidsgiver" },
+            { id: "faktum.hvilke-andre-ytelser.svar.dagpenger-annet-eos-land" },
+            { id: "faktum.hvilke-andre-ytelser.svar.annen-ytelse" },
+          ],
+          subFaktum: [
+            {
+              id: "faktum.tjenestepensjon-hvem-utbetaler",
+              type: "tekst",
+              requiredAnswerIds: [
+                "faktum.hvilke-andre-ytelser.svar.pensjon-offentlig-tjenestepensjon",
+              ],
+            },
+            {
+              id: "faktum.tjenestepensjon-hvilken-periode",
+              type: "periode",
+              requiredAnswerIds: [
+                "faktum.hvilke-andre-ytelser.svar.pensjon-offentlig-tjenestepensjon",
+              ],
+            },
+            {
+              id: "faktum.etterlonn-arbeidsgiver-hvem-utbetaler",
+              type: "tekst",
+              requiredAnswerIds: ["faktum.hvilke-andre-ytelser.svar.etterlonn-arbeidsgiver"],
+            },
+            {
+              id: "faktum.etterlonn-arbeidsgiver-hvilken-periode",
+              type: "periode",
+              requiredAnswerIds: ["faktum.hvilke-andre-ytelser.svar.etterlonn-arbeidsgiver"],
+            },
+            {
+              id: "faktum.dagpenger-hvilket-eos-land-utbetaler",
+              type: "land",
+              answerOptions: [], //todo: insert alle verdens land
+              requiredAnswerIds: ["faktum.hvilke-andre-ytelser.svar.dagpenger-annet-eos-land"],
+            },
+            {
+              id: "faktum.dagpenger-eos-land-hvilken-periode",
+              type: "periode",
+              requiredAnswerIds: ["faktum.hvilke-andre-ytelser.svar.dagpenger-annet-eos-land"],
+            },
+            {
+              id: "faktum.hvilken-annen-ytelse",
+              type: "tekst",
+              requiredAnswerIds: ["faktum.hvilke-andre-ytelser.svar.annen-ytelse"],
+            },
+            {
+              id: "faktum.annen-ytelse-hvem-utebetaler",
+              type: "tekst",
+              requiredAnswerIds: ["faktum.hvilke-andre-ytelser.svar.annen-ytelse"],
+            },
+            {
+              id: "faktum.annen-ytelse-hvilken-periode",
+              type: "periode",
+              requiredAnswerIds: ["faktum.hvilke-andre-ytelser.svar.annen-ytelse"],
+            },
+          ],
+          requiredAnswerIds: ["faktum.andre-ytelser-mottatt-eller-sokt.svar.ja"],
         },
       ],
     },
     {
-      id: "faktum.utbetaling-okonomisk-gode-tidligere-arbeidsgiver",
+      id: "faktum.utbetaling-eller-okonomisk-gode-tidligere-arbeidsgiver",
       type: "boolean",
       answerOptions: [
         {
-          id: "faktum.utbetaling-okonomisk-gode-tidligere-arbeidsgiver.svar.nei",
+          id: "faktum.utbetaling-eller-okonomisk-gode-tidligere-arbeidsgiver.svar.nei",
         },
-        { id: "faktum.utbetaling-okonomisk-gode-tidligere-arbeidsgiver.svar.ja" },
+        { id: "faktum.utbetaling-eller-okonomisk-gode-tidligere-arbeidsgiver.svar.ja" },
       ],
       subFaktum: [
         {
           id: "faktum.okonomisk-gode-tidligere-arbeidsgiver-hva-omfatter-avtalen",
           type: "tekst",
-          requiredAnswerIds: ["faktum.utbetaling-okonomisk-gode-tidligere-arbeidsgiver.svar.ja"],
+          requiredAnswerIds: [
+            "faktum.utbetaling-eller-okonomisk-gode-tidligere-arbeidsgiver.svar.ja",
+          ],
         },
       ],
     },
