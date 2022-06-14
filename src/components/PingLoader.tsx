9303b05a10b2891a2aedcb9af92c1232917e6fb0
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-06-14 15:44:18 +0200

    Used typography element from Aksel

--- src/components/PingLoader.tsx
+++ src/components/PingLoader.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./PingLoader.module.css";
-import { Loader } from "@navikt/ds-react";
+import { Label, Loader } from "@navikt/ds-react";
 
 export function PingLoader() {
   return (
     <div className={styles.pingLoader}>
       <Loader size="large" title="venter..." variant="interaction" />
       <div className={styles.loadingText}>
-        <span className={styles.heading}>Henter neste spørsmål</span>
+        <Label>Henter neste spørsmål</Label>
         <span>Tenk på noe som gjør deg glad.</span>
       </div>
     </div>
