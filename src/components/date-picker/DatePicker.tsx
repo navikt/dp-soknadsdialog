c49d4b0b3b396a975e17ff7a2945d6e96baabb97
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-07-26 11:29:41 +0200

    New datepicker

--- src/components/date-picker/DatePicker.tsx
+++ src/components/date-picker/DatePicker.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { format, isValid } from "date-fns";
+import { TextField } from "@navikt/ds-react";
+import dateFnsFormat from "date-fns/format";
 
 interface DatePickerProps {
   label: string;
@@ -14,17 +17,38 @@ export function DatePicker(props: DatePickerProps) {
   const [date, setDate] = useState<Date | undefined>(
     props.value ? new Date(props.value) : undefined
   );
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+  const [isValidDate, setIsValidDate] = useState(true);
 
-  function onDateChange(value: Date | undefined) {
-    if (value) {
+  function onDateChange(value: Date) {
+    console.log("Just value:", value);
+    if (isValid(value)) {
+      setIsValidDate(true);
+      console.log("Valid:", value);
       setDate(value);
       props.onChange(value);
+    } else {
+      setIsValidDate(false);
     }
   }
 
   return (
     <>
-      <DayPicker mode="single" required={true} selected={date} onSelect={onDateChange} />
+      <div>
+        <TextField
+          label={props.label}
+          type={"text"}
+          placeholder={format(new Date(), "y-MM-dd")}
+          defaultValue={date ? dateFnsFormat(date, "dd.MM.yyyy") : ""}
+          onClick={() => setIsDatePickerOpen(!isDatePickerOpen)}
+          onChange={(event) => onDateChange(new Date(event.currentTarget.value))}
+          error={!isValidDate ? "Ugyldig dato" : null}
+        />
+
+        {isDatePickerOpen && (
+          <DayPicker mode="single" defaultMonth={date} selected={date} onSelect={onDateChange} />
+        )}
+      </div>
     </>
   );
 }
