a535103a4420a3f2a10096fd5a849cc0ce276184
Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
2022-08-16 13:59:58 +0200

    Norskifiserer svar-feltet, viser felt for begrunnelse

--- src/components/dokumentkrav/Dokumentkrav.tsx
+++ src/components/dokumentkrav/Dokumentkrav.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Heading, Radio, RadioGroup } from "@navikt/ds-react";
+import { Button, Heading, Radio, RadioGroup, TextField } from "@navikt/ds-react";
 import { IDokumentkrav, IFileState, IDokumentkravFil } from "../../types/documentation.types";
 import { FileUploader } from "../file-uploader/FileUploader";
 import { FileList } from "../file-uploader/FileList";
@@ -16,11 +16,14 @@ interface IProps {
 
 export const TRIGGER_FILE_UPLOAD = "dokumentkrav.svar.send.inn.naa";
 const DOKUMENTKRAV_DESCRIPTION = "faktum.arbeidsforhold.navn-bedrift";
+const TRIGGER_BEGRUNNELSE = "dokumentkrav.svar.send.inn.sender.ikke";
 
 export function Dokumentkrav(props: IProps) {
   const { dokumentkrav } = props;
-  const [answer, setAnswer] = useState(dokumentkrav.svar || "");
+  const [svar, setSvar] = useState(dokumentkrav.svar || "");
   const [handledFiles, setHandlesFiles] = useState<IFileState[]>([]);
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  const [begrunnelse, setBegrunnelse] = useState(dokumentkrav.begrunnelse || "");
   const { getFaktumTextById, getSvaralternativTextById, getAppTekst } = useSanity();
   const kravText = getFaktumTextById(dokumentkrav.beskrivendeId);
   const [alertText, setAlertText] = useState<ISanityAlertText>();
@@ -30,10 +33,10 @@ export function Dokumentkrav(props: IProps) {
   });
 
   useEffect(() => {
-    if (answer !== "") {
-      setAlertText(getSvaralternativTextById(answer)?.alertText);
+    if (svar !== "") {
+      setAlertText(getSvaralternativTextById(svar)?.alertText);
     }
-  }, [answer]);
+  }, [svar]);
 
   function sendDocuments() {
     alert("TODO: Send inn svar");
@@ -53,8 +56,8 @@ export function Dokumentkrav(props: IProps) {
 
       <RadioGroup
         legend={getAppTekst("dokumentkrav.velg.svaralternativ")}
-        onChange={setAnswer}
-        value={answer}
+        onChange={setSvar}
+        value={svar}
       >
         {dokumentkrav.gyldigeValg.map((textId) => {
           const svaralternativText = getSvaralternativTextById(textId);
@@ -72,13 +75,24 @@ export function Dokumentkrav(props: IProps) {
         <HelpText className={styles.helpTextSpacing} helpText={kravText.helpText} />
       )}
 
-      {answer === TRIGGER_FILE_UPLOAD && (
+      {svar === TRIGGER_FILE_UPLOAD && (
         <>
           <FileUploader id={dokumentkrav.id} onHandle={setHandlesFiles} />
           <FileList previouslyUploaded={uploadedFiles} handledFiles={handledFiles} />
         </>
       )}
-      {answer !== "" && <Button onClick={sendDocuments}>Send inn</Button>}
+
+      {svar === TRIGGER_BEGRUNNELSE && (
+        <TextField
+          defaultValue={dokumentkrav.begrunnelse}
+          label={getAppTekst("dokumentkrav.sender.ikke.begrunnelse")}
+          size="medium"
+          type="text"
+          onChange={(event) => setBegrunnelse(event?.currentTarget.value)}
+        />
+      )}
+
+      {svar !== "" && <Button onClick={sendDocuments}>Send inn</Button>}
     </div>
   );
 }
