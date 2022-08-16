9cc842543622290dc105abafa735936803dcdca0
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-08-16 16:25:21 +0200

    Environment satt i yaml filer

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,8 +1,13 @@
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_ENV = process.env.SENTRY_ENV || "";
+
+/* eslint-disable no-console*/
+console.log("SENTRY_ENV:", SENTRY_ENV);
 
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
+  environment: SENTRY_ENV,
 });
