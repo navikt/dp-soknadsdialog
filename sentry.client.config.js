6a15f3f649900570e618d8d320858e6ce2f0b3dc
Amund Øgar Meisal <amund.ogar.meisal@nav.no>
2022-08-18 16:10:00 +0200

    Fjernet console log

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,9 +5,6 @@ const { publicRuntimeConfig } = getConfig();
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 const SENTRY_STAGE = publicRuntimeConfig.NEXT_PUBLIC_SENTRY_STAGE || "";
 
-/* eslint-disable no-console*/
-console.log("SENTRY_STAGE:", SENTRY_STAGE);
-
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
