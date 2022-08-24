438f430b61f0d1a1abf909f3fdb9a9720e16329f
André Roaldseth <andre.roaldseth@nav.no>
2022-08-24 12:59:56 +0200

    Logg manglende tekster som en custom error
    Da kan vi enklere skille ut manglende tekster i Sentry og ta ut rapporter på hvilke tekster som mangler. Vi får også med context om hvilken locale som er i bruk

--- src/sentry.logger.ts
+++ src/sentry.logger.ts
@@ -1,5 +1,12 @@
 import * as Sentry from "@sentry/nextjs";
 
 export function logMissingSanityText(textId: string) {
-  Sentry.captureEvent({ message: `Fant ikke seksjonsTekst med id: ${textId}` });
+  Sentry.captureException(new MissingTextException(`Mangler tekst for "${textId}"`));
+}
+
+class MissingTextException extends Error {
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
 }
