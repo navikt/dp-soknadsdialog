9b0d18223660d90cb33de7aa4c88fb42a4300745
Sindre Sægrov <sindre.segrov@nav.no>
2022-09-02 13:12:56 +0200

    Tester filnedlasting i dev

--- src/components/file-list/FileListItem.tsx
+++ src/components/file-list/FileListItem.tsx
@@ -5,11 +5,37 @@ import styles from "./FileListItem.module.css";
 import api from "../../api.utils";
 
 export function FileListItem(props: IDokumentkravFil) {
+  async function handleDownloadFile() {
+    try {
+      const response = await fetch(api(`/documentation/file/${props.filsti}/download`));
+      // eslint-disable-next-line no-console
+      console.log(response);
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const blob = await response.blob();
+      // eslint-disable-next-line no-console
+      console.log("blob: ", blob);
+
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = props.filnavn;
+      a.click();
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    }
+  }
+
   return (
     <li className={styles.fileItem}>
       <Link href={api(`/documentation/file/${props.filsti}/download`)} download={props.filnavn}>
         <BodyShort size="medium">{props.filnavn}</BodyShort>
       </Link>
+      <button onClick={() => handleDownloadFile()}>Last ned</button>
     </li>
   );
 }
