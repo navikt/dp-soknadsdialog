62fd57fba8aa2a775a63e5aa06b7dde431cf0a83
Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
2022-09-11 20:33:04 +0200

    Legger tekst per periode som appTekst, styler FaktumPeriode

--- src/components/faktum/FaktumPeriode.tsx
+++ src/components/faktum/FaktumPeriode.tsx
@@ -10,6 +10,7 @@ import { BodyShort, Fieldset, Label } from "@navikt/ds-react";
 import { HelpText } from "../HelpText";
 import styles from "./Faktum.module.css";
 import periodeStyles from "./FaktumPeriode.module.css";
+import { DateTimeFormatOptions } from "next-intl";
 
 export function FaktumPeriode(props: IFaktum<IQuizPeriodeFaktum>) {
   const { faktum, onChange } = props;
@@ -20,8 +21,8 @@ export function FaktumPeriode(props: IFaktum<IQuizPeriodeFaktum>) {
   const beskrivendeIdTil = `${props.faktum.beskrivendeId}.til`;
 
   const faktumTexts = getFaktumTextById(props.faktum.beskrivendeId);
-  const faktumTextsFra = getAppTekst(beskrivendeIdFra);
-  const faktumTextsTil = getAppTekst(beskrivendeIdTil);
+  const faktumTextFra = getAppTekst(beskrivendeIdFra);
+  const faktumTextTil = getAppTekst(beskrivendeIdTil);
 
   const [svar, setSvar] = useState<IQuizPeriodeFaktumAnswerType | undefined>(props.faktum.svar);
 
@@ -49,14 +50,35 @@ export function FaktumPeriode(props: IFaktum<IQuizPeriodeFaktum>) {
     saveFaktumToQuiz(faktum, svar);
   }
 
+  function parseDate(date: string) {
+    const options: DateTimeFormatOptions = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(date).toLocaleDateString("no-NB", options);
+  }
+
   if (props.faktum.readOnly || props.readonly) {
     return (
-      <>
-        <Label>{faktumTexts ? faktumTexts.text : faktum.beskrivendeId}</Label>
-        <BodyShort>
-          {svar?.fom} – {svar?.tom}
-        </BodyShort>
-      </>
+      <div className={periodeStyles.faktumPeriode}>
+        <Label className={periodeStyles.readOnlyTittel}>
+          {faktumTexts ? faktumTexts.text : faktum.beskrivendeId}
+        </Label>
+        {svar?.fom && (
+          <div className={periodeStyles.faktumPeriodeFra}>
+            <Label>{faktumTextFra}</Label>
+            <BodyShort>{parseDate(svar?.fom)}</BodyShort>
+          </div>
+        )}
+        {svar?.tom && (
+          <div>
+            <Label>{faktumTextTil}</Label>
+            <BodyShort>{parseDate(svar?.tom)}</BodyShort>
+          </div>
+        )}
+      </div>
     );
   }
 
@@ -76,7 +98,7 @@ export function FaktumPeriode(props: IFaktum<IQuizPeriodeFaktum>) {
         <div className={periodeStyles.faktumPeriodeFra}>
           <DatePicker
             id={beskrivendeIdFra}
-            label={faktumTextsFra}
+            label={faktumTextFra}
             onChange={onFromDateSelection}
             value={svar?.fom}
           />
@@ -84,7 +106,7 @@ export function FaktumPeriode(props: IFaktum<IQuizPeriodeFaktum>) {
         <div>
           <DatePicker
             id={beskrivendeIdTil}
-            label={faktumTextsTil}
+            label={faktumTextTil}
             disabled={!svar?.fom}
             onChange={onToDateSelection}
             value={svar?.tom}
