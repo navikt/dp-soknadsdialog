15af907e4c49e28a76e1dc117152862620a6d09f
André Roaldseth <andre.roaldseth@nav.no>
2022-09-22 11:30:31 +0200

    Legg på logging av tid brukt ved besvaring av faktum

--- src/pages/api/soknad/[uuid]/faktum/[faktumId].ts
+++ src/pages/api/soknad/[uuid]/faktum/[faktumId].ts
@@ -3,6 +3,7 @@ import { getSession } from "@navikt/dp-auth/server";
 import { audienceDPSoknad } from "../../../../../api.utils";
 import { withSentry } from "@sentry/nextjs";
 
+/* eslint-disable no-console */
 const saveFaktumHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     query: { uuid, faktumId },
@@ -13,10 +14,15 @@ const saveFaktumHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     return res.status(200).json({ status: "ok" });
   }
 
+  console.time("getSession");
   const { token, apiToken } = await getSession({ req });
+  console.timeEnd("getSession");
 
   if (token && apiToken) {
+    console.time("onBehalfOfToken");
     const onBehalfOfToken = await apiToken(audienceDPSoknad);
+    console.timeEnd("onBehalfOfToken");
+    console.time("putFaktumSvar");
     const response: Response = await fetch(
       `${process.env.API_BASE_URL}/soknad/${uuid}/faktum/${faktumId}`,
       {
@@ -28,6 +34,7 @@ const saveFaktumHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         body,
       }
     );
+    console.timeEnd("putFaktumSvar");
 
     return res.status(response.status).json(await response.json());
   } else {
