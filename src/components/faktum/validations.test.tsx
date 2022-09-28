9d188dbe7f1b788ed946f34b3caa93d71282213b
Nattaphong Klinjan <110396878+nattaphongklinjan@users.noreply.github.com>
2022-09-28 12:51:58 +0200

    DAG-447: Legg til bedre validering på faktum number (#72)

--- src/components/faktum/validations.test.tsx
+++ src/components/faktum/validations.test.tsx
@@ -6,7 +6,6 @@ import {
   isValidTextLength,
   isValidYearRange,
   isValidDateYear,
-  isValidNumber,
 } from "./validations";
 
 const validTextLengthMock =
@@ -15,13 +14,6 @@ const inValidTextLengthMock =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like.";
 
 describe("Input validation", () => {
-  test("Validate number", async () => {
-    expect(isValidNumber(30)).toBe(true);
-    expect(isValidNumber(-20)).toBe(false);
-    expect(isValidNumber(parseFloat("30,5"))).toBe(true);
-    expect(isValidNumber(-37.5)).toBe(false);
-  });
-
   test("Validate text length", async () => {
     expect(isValidTextLength(validTextLengthMock)).toBe(true);
     expect(isValidTextLength(inValidTextLengthMock)).toBe(false);
