181a90b86624745a2fc06763fc74ceafb1e90c2b
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2022-09-28 12:46:54 +0200

    DAG-447: Legg til bedre validering på faktum number

--- src/components/faktum/validations.ts
+++ src/components/faktum/validations.ts
@@ -1,24 +1,17 @@
 import { MAX_TEXT_LENGTH, YEAR_RANGE } from "./../../constants";
 import { addWeeks, addYears } from "date-fns";
 
-export function isValidNumber(value: number | string): boolean {
-  const validNumber = /^[0-9]{1,2}([,.][0-9]{1,2})?$/;
-  return validNumber.test(value.toString());
-}
-
 export function isValidTextLength(value: string): boolean {
   const maxTextLength = MAX_TEXT_LENGTH;
   return value.length <= maxTextLength;
 }
 
 export function isValidArbeidstimer(value: number): boolean {
-  const validNumber = isValidNumber(value);
-  return validNumber && value >= 0 && value <= 99;
+  return value >= 0 && value <= 99;
 }
 
 export function isValidPermitteringsPercent(value: number): boolean {
-  const validNumber = isValidNumber(value);
-  return validNumber && value >= 0 && value <= 100;
+  return value >= 0 && value <= 100;
 }
 
 export function isValidDateYear(date: Date): boolean {
