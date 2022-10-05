54c5b6272ffbeba81a1603ff96657cd2a12a9801
Sindre Sægrov <sindre.segrov@nav.no>
2022-10-05 12:32:44 +0200

    Test i dev

--- src/api/arbeidssoker-api.ts
+++ src/api/arbeidssoker-api.ts
@@ -1,6 +1,7 @@
 import { v4 as uuid } from "uuid";
 import { getSession } from "@navikt/dp-auth/server";
 import { GetServerSidePropsContext } from "next/types";
+import { formatISO } from "date-fns";
 
 const periodeFormatter = new Intl.DateTimeFormat("no", {
   year: "numeric",
@@ -16,19 +17,34 @@ export async function getArbeidssokerStatus({ req }: GetServerSidePropsContext) 
   // eslint-disable-next-line no-console
   console.log(formaterDato(new Date()));
 
+  const newDate = new Date();
+  const oldFormat = formaterDato(newDate);
+  const newFormat = formatISO(newDate, { representation: "date" });
+
+  // eslint-disable-next-line no-console
+  console.log("oldFormat: ", oldFormat);
+  // eslint-disable-next-line no-console
+  console.log("newFormat: ", newFormat);
+
   const { payload } = await getSession({ req });
   const idtoken = req.cookies["selvbetjening-idtoken"];
 
   const callId = uuid();
-  const url = `${process.env.VEILARBPROXY_URL}?fnr=${payload?.pid}&fraOgMed=${formaterDato(
-    new Date()
-  )}`;
+  const url = `${process.env.VEILARBPROXY_URL}?fnr=${payload?.pid}&fraOgMed=${oldFormat}`;
 
-  return await fetch(url.toString(), {
+  // eslint-disable-next-line no-console
+  console.log("getArbeidssokerStatus() URL: ", url);
+
+  const response = await fetch(url.toString(), {
     headers: {
       cookie: `selvbetjening-idtoken=${idtoken}`,
       "Nav-Consumer-Id": "dp-soknadsdialog",
       "Nav-Call-Id": callId,
     },
   });
+
+  // eslint-disable-next-line no-console
+  console.log("Response: ", response);
+
+  return response;
 }
