afdd82ec8f5e154ab47c79a2afe754c277dc6a9a
Sindre Sægrov <sindre.segrov@nav.no>
2022-10-05 12:44:29 +0200

    Remove old date formating

--- src/api/arbeidssoker-api.ts
+++ src/api/arbeidssoker-api.ts
@@ -3,34 +3,14 @@ import { getSession } from "@navikt/dp-auth/server";
 import { GetServerSidePropsContext } from "next/types";
 import { formatISO } from "date-fns";
 
-const periodeFormatter = new Intl.DateTimeFormat("no", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-});
-
-function formaterDato(date: Date) {
-  return periodeFormatter.format(date).split(".").reverse().join("-");
-}
-
 export async function getArbeidssokerStatus({ req }: GetServerSidePropsContext) {
-  // eslint-disable-next-line no-console
-  console.log(formaterDato(new Date()));
-
-  const newDate = new Date();
-  const oldFormat = formaterDato(newDate);
-  const newFormat = formatISO(newDate, { representation: "date" });
-
-  // eslint-disable-next-line no-console
-  console.log("oldFormat: ", oldFormat);
-  // eslint-disable-next-line no-console
-  console.log("newFormat: ", newFormat);
+  const today = formatISO(new Date(), { representation: "date" });
 
   const { payload } = await getSession({ req });
   const idtoken = req.cookies["selvbetjening-idtoken"];
 
   const callId = uuid();
-  const url = `${process.env.VEILARBPROXY_URL}?fnr=${payload?.pid}&fraOgMed=${oldFormat}`;
+  const url = `${process.env.VEILARBPROXY_URL}?fnr=${payload?.pid}&fraOgMed=${today}`;
 
   // eslint-disable-next-line no-console
   console.log("getArbeidssokerStatus() URL: ", url);
