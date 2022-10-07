bfc9abcf789513810fdba911e1be110207a410ac
Sindre Sægrov <sindre.segrov@nav.no>
2022-10-07 14:50:23 +0200

    Fjern debug log

--- src/api/arbeidssoker-api.ts
+++ src/api/arbeidssoker-api.ts
@@ -5,19 +5,10 @@ import { formatISO } from "date-fns";
 
 export async function getArbeidssokerStatus({ req }: GetServerSidePropsContext) {
   const today = formatISO(new Date(), { representation: "date" });
-
   const { payload, token } = await getSession({ req });
-  const idtoken = req.cookies["selvbetjening-idtoken"];
 
   const callId = uuid();
   const url = `${process.env.VEILARBPROXY_URL}?fnr=${payload?.pid}&fraOgMed=${today}`;
-
-  // eslint-disable-next-line no-console
-  console.log("getArbeidssokerStatus() URL: ", url);
-  // eslint-disable-next-line no-console
-  console.log("idtoken: ", idtoken);
-  // eslint-disable-next-line no-console
-  console.log("token: ", token);
 
   return await fetch(url, {
     headers: {
