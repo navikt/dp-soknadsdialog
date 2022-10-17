3b469bdd1d166110e18bceb04c5aeaf2ef7bb08c
Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
2022-10-17 13:09:33 +0200

    DAG-172: Fjerner TODOs som jeg har funnet ut av

--- src/types/personalia.types.ts
+++ src/types/personalia.types.ts
@@ -2,7 +2,7 @@ export interface IPersonalia {
   forNavn: string;
   mellomNavn: string;
   etterNavn: string;
-  fødselsDato: string; // TODO: På skissene vises fødselsnummer, mens det ikke kommer med fra backenden. Er det noe som skal være med?
+  fødselsDato: string;
   postAdresse?: IAdresse;
   folkeregistrertAdresse?: IAdresse;
   kontonummer?: string;
@@ -15,7 +15,7 @@ export interface IAdresse {
   adresselinje1: string;
   adresselinje2: string;
   adresselinje3: string;
-  byEllerStedsnavn?: string; // TODO: Finne ut om denne skal være med, feltes ble ikke med i respons fra dev
+  byEllerStedsnavn?: string;
   landkode: string;
   land: string;
   postnummer: string;
