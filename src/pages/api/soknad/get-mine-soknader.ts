1d1ce865118b76b3f817953624163990c2d5a7f9
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2022-10-20 15:59:50 +0200

    DAG-514: Debug Inngang side i dev runde2

--- src/pages/api/soknad/get-mine-soknader.ts
+++ src/pages/api/soknad/get-mine-soknader.ts
@@ -15,8 +15,7 @@ function formaterDato(date: Date) {
 }
 
 export function getMineSoknader(onBehalfOfToken: string) {
-  // Todo
-  // Finn ut hvor dager tilbake i tid vi skal ha
+  // Finn ut hvor mange dager tilbake i tid vi skal ha
   const soknadFomDate = subDays(Date.now(), 7);
 
   const url = `${process.env.API_BASE_URL}/soknad/mine-soknader?fom=${formaterDato(soknadFomDate)}`;
