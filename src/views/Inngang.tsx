1696d20fd3a19ec7e767e700724ffb74c59a4ba5
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2022-10-21 14:04:33 +0200

    Debug Inngang-side i DEV, debugger inngang component

--- src/views/Inngang.tsx
+++ src/views/Inngang.tsx
@@ -1,12 +1,15 @@
-import { InngangPaabegynt } from "../components/inngang-paabegynt/InngangPaabegynt";
-import { InngangSendDocument } from "../components/inngang-send-document/InngangSendDocument";
+// import { InngangPaabegynt } from "../components/inngang-paabegynt/InngangPaabegynt";
+// import { InngangSendDocument } from "../components/inngang-send-document/InngangSendDocument";
 import { IMineSoknader } from "../types/quiz.types";
 
-export function Inngang({ paabegynt, innsendte }: IMineSoknader) {
+export function Inngang(props: IMineSoknader) {
+  // eslint-disable-next-line no-console
+  console.log(props);
   return (
     <main>
-      {paabegynt && <InngangPaabegynt {...paabegynt} />}
-      {innsendte && <InngangSendDocument innsendte={innsendte} />}
+      Inngang
+      {/* {paabegynt && <InngangPaabegynt {...paabegynt} />} */}
+      {/* {innsendte && <InngangSendDocument innsendte={innsendte} />} */}
     </main>
   );
 }
