79d17115babcb8731b04d78d638e0d0d85bcbdea
André Roaldseth <andre.roaldseth@nav.no>
2022-10-27 17:05:48 +0200

    Sørg for at ny auth fungerer lokalt

--- src/auth.utils.ts
+++ src/auth.utils.ts
@@ -1,13 +1,26 @@
-import { makeSession } from "@navikt/dp-auth";
+import { GetSessionWithOboProvider, makeSession } from "@navikt/dp-auth";
 import { idporten } from "@navikt/dp-auth/identity-providers";
 import { tokenX, withInMemoryCache } from "@navikt/dp-auth/obo-providers";
 
-export const getSession = makeSession({
-  identityProvider: idporten,
-  oboProvider: withInMemoryCache(tokenX, {
-    // eslint-disable-next-line no-console
-    cacheHit: () => console.log(`TokenX cache hit`),
-    // eslint-disable-next-line no-console
-    cacheMiss: () => console.log(`TokenX cache miss`),
-  }),
-});
+let getSession: GetSessionWithOboProvider;
+
+if (process.env.AUTH_PROVIDER == "local") {
+  const staticToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+  getSession = makeSession({
+    identityProvider: async () => staticToken,
+    oboProvider: async (token: string, audience: string) => token + audience,
+  });
+} else {
+  getSession = makeSession({
+    identityProvider: idporten,
+    oboProvider: withInMemoryCache(tokenX, {
+      // eslint-disable-next-line no-console
+      cacheHit: () => console.log(`TokenX cache hit`),
+      // eslint-disable-next-line no-console
+      cacheMiss: () => console.log(`TokenX cache miss`),
+    }),
+  });
+}
+
+export { getSession };
