acdb6106a0be3d8b7bcdcd9a72961875a0ad91f4
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2022-10-31 15:51:40 +0100

    DAG-537: Oppdatert url til favicon filer

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -65,9 +65,22 @@ export default class MyDocument extends Document<DecoratorComponents> {
     return (
       <Html lang={locale}>
         <Head>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="shortcut icon" href="/favicon/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon/favicon-32x32.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon/favicon-16x16.png`}
+          />
+          <link
+            rel="shortcut icon"
+            href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon/favicon.ico`}
+          />
           <Styles />
         </Head>
         <body>
