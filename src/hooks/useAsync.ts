8891a8072a9dd8bc244b79888bd9afcb093e9ae9
Sindre Sægrov <sindre.segrov@nav.no>
2022-11-04 16:42:25 +0100

    Fjern kommentarer

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -13,10 +13,6 @@ export function useAsync<T>(asyncFunction: () => Promise<Response>, immediate = 
     setStatus("idle");
   }
 
-  // The execute function wraps asyncFunction and
-  // handles setting state for pending, value, and error.
-  // useCallback ensures the below useEffect is not called
-  // on every render, but only if asyncFunction changes.
   const execute = useCallback(async () => {
     setStatus("pending");
     setValue(null);
@@ -36,9 +32,6 @@ export function useAsync<T>(asyncFunction: () => Promise<Response>, immediate = 
     }
   }, [asyncFunction]);
 
-  // Call execute if we want to fire it right away.
-  // Otherwise execute can be called later, such as
-  // in an onClick handler.
   useEffect(() => {
     if (immediate) {
       execute();
