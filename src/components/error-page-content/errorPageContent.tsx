c9013e5040aea66ccfb64604db01c5a5c7786021
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2022-11-11 15:28:36 +0100

    Lagt til fallback feilmelding

--- src/components/error-page-content/errorPageContent.tsx
+++ src/components/error-page-content/errorPageContent.tsx
@@ -9,9 +9,9 @@ export function ErrorPageContent({ title, details }: IProps) {
   return (
     <main>
       <Heading level="1" size="xlarge" spacing>
-        {title}
+        {title ?? "Det har skjedd en teknisk feil"}
       </Heading>
-      <BodyLong>{details}</BodyLong>
+      <BodyLong>{details ?? "Beklager, vi mistet kontakten med systemene våre."}</BodyLong>
       <Link href="https://www.nav.no/no/ditt-nav" passHref>
         <Button className="my-6" variant="primary" as="a" size="medium">
           Gå til Ditt NAV
