c5f407ec4cf5b0b44ce2b000bcc912b5fb7ebdca
André Roaldseth <andre.roaldseth@nav.no>
2022-11-15 19:40:25 +0100

    Fjern tekst om å dra når man er på mobil

--- src/components/file-uploader/FileUploader.tsx
+++ src/components/file-uploader/FileUploader.tsx
@@ -13,6 +13,7 @@ interface IProps {
   handleUploadedFiles: (file: IDokumentkravFil) => void;
   maxFileSize: number;
 }
+
 interface IFileError {
   fileName: string;
   error: "INVALID_FILE_FORMAT" | "INVALID_FILE_SIZE" | "SERVER_ERROR";
@@ -67,21 +68,21 @@ export function FileUploader({ dokumentkrav, handleUploadedFiles, maxFileSize }:
   }, []);
 
   const { getRootProps, getInputProps } = useDropzone({
-    onDrop
+    onDrop,
   });
   return (
     <>
       <div {...getRootProps()} className={styles.fileUploader}>
         <input data-testid="dropzone" {...getInputProps()} />
 
-        <p>
-          <b>{getAppText("filopplaster.tekst.dra-filene-hit")}</b>
-          <br />
-          {getAppText("filopplaster.tekst.eller")}
-        </p>
-        <Button loading={isLoading}>
-          {getAppText("filopplaster.knapp.velg-filer")}
-        </Button>
+        <span className={styles.withHover}>
+          <p>
+            <b>{getAppText("filopplaster.tekst.dra-filene-hit")}</b>
+            <br />
+            {getAppText("filopplaster.tekst.eller")}
+          </p>
+        </span>
+        <Button loading={isLoading}>{getAppText("filopplaster.knapp.velg-filer")}</Button>
       </div>
 
       {errors.length > 0 && (
