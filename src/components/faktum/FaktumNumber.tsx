0bd4fea13e53d362a203fe64e5102f977da8fbf9
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2022-11-15 14:25:38 +0100

    Fikset input number, lagt til maks 9 siffer, replace komma med punktum før sendes til backend og omvent ved visning

--- src/components/faktum/FaktumNumber.tsx
+++ src/components/faktum/FaktumNumber.tsx
@@ -61,7 +61,9 @@ export function FaktumNumber(props: IFaktum<IQuizNumberFaktum>) {
       }
       case "double": {
         setIsValid(true);
-        debouncedChange(parseFloat(value));
+        // Replaced comma with dot
+        const formattedValue = value.replace(/,/g, ".");
+        debouncedChange(parseFloat(formattedValue));
         break;
       }
 
@@ -144,15 +146,19 @@ export function FaktumNumber(props: IFaktum<IQuizNumberFaktum>) {
     }
   }
 
+  // Replaced dot with comma
+  const displayValue = debouncedValue?.toString().replace(/\./g, ",");
+
   return (
     <>
       <TextField
         className={styles.faktumNumber}
-        defaultValue={debouncedValue?.toString()}
+        defaultValue={displayValue}
         label={faktumTexts?.text ? faktumTexts.text : faktum.beskrivendeId}
         description={faktumTexts?.description && <PortableText value={faktumTexts.description} />}
         size="medium"
         type="text"
+        maxLength={9}
         inputMode="numeric"
         onChange={onValueChange}
         onBlur={debouncedChange.flush}
