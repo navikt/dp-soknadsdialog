8f3941e53436fd5efe98afe9c3408bc5ade8c421
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2022-11-16 15:39:50 +0100

    FaktumNumber med egen valideringshook

---
+++ src/hooks/faktum/useFaktumNumberValidation.ts
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import {
+  isValidArbeidstimer,
+  isValidPermitteringsPercent,
+} from "../../components/faktum/validation/validations.utils";
+import { useSanity } from "../../context/sanity-context";
+import { useValidation } from "../../context/validation-context";
+import { QuizFaktum } from "../../types/quiz.types";
+
+type NumberValidationErrorTypes = "negativeValue" | "invalidValue" | "notNumber" | "emptyValue";
+
+interface IProps {
+  setHasError: (hasError: boolean | NumberValidationErrorTypes) => void;
+  getErrorMessage: (faktum: QuizFaktum) => string | undefined;
+  isValidInput: (value: number | null) => boolean;
+}
+
+export function useValidateFaktumNumber(faktumBeskrivendeId: string): IProps {
+  const { getAppText, getFaktumTextById } = useSanity();
+  const { unansweredFaktumId } = useValidation();
+  const faktumTexts = getFaktumTextById(faktumBeskrivendeId);
+  const [hasError, setHasError] = useState<boolean | NumberValidationErrorTypes>(false);
+
+  function isValidInput(value: number | null) {
+    if (faktumBeskrivendeId === "faktum.arbeidsforhold.permittert-prosent") {
+      if (value === null) {
+        setHasError("emptyValue");
+        return false;
+      }
+
+      const isValid = isValidPermitteringsPercent(value);
+      setHasError(!isValid ? "invalidValue" : false);
+      return isValid;
+    }
+
+    if (faktumBeskrivendeId === "faktum.arbeidsforhold.antall-timer-dette-arbeidsforhold") {
+      if (value === null) {
+        setHasError("emptyValue");
+        return false;
+      }
+
+      const isValid = isValidArbeidstimer(value);
+      setHasError(!isValid ? "invalidValue" : false);
+      return isValid;
+    }
+
+    if (value === null || value === 0) {
+      setHasError(false);
+      return true;
+    }
+
+    if (value < 0) {
+      setHasError("negativeValue");
+      return false;
+    }
+
+    setHasError(false);
+    return true;
+  }
+
+  function getErrorMessageByErrorType() {
+    switch (hasError) {
+      case "emptyValue":
+        return getAppText("validering.number-faktum.tom-svar");
+      case "negativeValue":
+        return getAppText("validering.number-faktum.ikke-negativt-tall");
+      case "notNumber":
+        return getAppText("validering.number-faktum.maa-vaere-et-tall");
+      case "invalidValue":
+        return faktumTexts?.errorMessage ?? getAppText("validering.number-faktum.ugyldig");
+      default:
+        return undefined;
+    }
+  }
+
+  function getErrorMessage(faktum: QuizFaktum) {
+    if (unansweredFaktumId === faktum.id) {
+      return getAppText("validering.faktum.ubesvart");
+    } else if (hasError) {
+      return getErrorMessageByErrorType();
+    } else {
+      undefined;
+    }
+  }
+
+  return {
+    setHasError,
+    getErrorMessage,
+    isValidInput,
+  };
+}
