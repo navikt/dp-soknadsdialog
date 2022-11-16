cc1fffd3035e2e4d9fe38f256f93ab6c3281433e
Sindre Sægrov <sindre.segrov@nav.no>
2022-11-16 15:46:48 +0100

    Legg til read only på section og lag PDF view

---
+++ src/pages/soknad/[uuid]/pdf.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next/types";
+import { QuizProvider } from "../../../context/quiz-context";
+import { ValidationProvider } from "../../../context/validation-context";
+import { audienceDPSoknad } from "../../../api.utils";
+import { getSoknadState } from "../../api/quiz-api";
+import ErrorPage from "../../_error";
+import { mockNeste } from "../../../localhost-data/mock-neste";
+import { IQuizState } from "../../../types/quiz.types";
+import { getSession } from "../../../auth.utils";
+import { Pdf } from "../../../views/pdf/Pdf";
+
+interface IProps {
+  soknadState: IQuizState | null;
+  errorCode: number | null;
+}
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<IProps>> {
+  const { query, locale } = context;
+  const uuid = query.uuid as string;
+
+  if (process.env.NEXT_PUBLIC_LOCALHOST) {
+    return {
+      props: {
+        soknadState: mockNeste,
+        errorCode: null,
+      },
+    };
+  }
+
+  const session = await getSession(context.req);
+  if (!session) {
+    return {
+      redirect: {
+        destination: locale ? `/oauth2/login?locale=${locale}` : "/oauth2/login",
+        permanent: false,
+      },
+    };
+  }
+
+  let errorCode = null;
+  let soknadState = null;
+
+  const onBehalfOfToken = await session.apiToken(audienceDPSoknad);
+  const soknadStateResponse = await getSoknadState(uuid, onBehalfOfToken);
+
+  if (!soknadStateResponse.ok) {
+    errorCode = soknadStateResponse.status;
+  } else {
+    soknadState = await soknadStateResponse.json();
+  }
+
+  return {
+    props: {
+      soknadState,
+      errorCode,
+    },
+  };
+}
+
+export default function SoknadPage(props: IProps) {
+  if (props.errorCode || !props.soknadState) {
+    return (
+      <ErrorPage
+        title="Vi har tekniske problemer akkurat nå"
+        details="Beklager, vi får ikke kontakt med systemene våre. Svarene dine er lagret og du kan prøve igjen om litt."
+        statusCode={props.errorCode || 500}
+      />
+    );
+  }
+
+  return (
+    <QuizProvider initialState={props.soknadState}>
+      <ValidationProvider>
+        <Pdf />
+      </ValidationProvider>
+    </QuizProvider>
+  );
+}
