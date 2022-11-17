e16efc4a01014e4ace3a952e4cc6c527dbbec3b2
André Roaldseth <andre.roaldseth@nav.no>
2022-11-17 15:57:45 +0100

    Grovt utkast til lokal koding mot dp-soknad i dev

---
+++ scripts/local-auth.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+# Anbefalte tools
+# kubectl
+# kubens
+# kubectx
+
+cluster=$(kubectx -c)
+env_file="$script_dir/../.env.local"
+
+if [[ $cluster != "dev-gcp" ]]
+then
+  die "Støtter kun dev-gcp som cluster, kjør kubectx dev-gcp for å bytte"
+fi
+
+# gcloud auth login
+
+# Logg inn i browser
+URL=$(grep ingress "$script_dir/../.nais/vars-dev.yaml" | awk '{print $2}')
+echo "Gå til $URL og logg inn"
+echo "Hent token fra $URL/api/internal/token"
+
+read -sp "Paste inn token: " active_token
+
+echo "NEXT_PUBLIC_LOCALHOST=false" > $env_file
+echo "LOCAL_TOKEN=$active_token" >> $env_file
+
+pod=$(kubectl get pods -o jsonpath={..metadata.name} -l app=dp-soknadsdialog | awk '{print $1}')
+kubectl exec $pod -- env | grep TOKEN_X -i >> $env_file
