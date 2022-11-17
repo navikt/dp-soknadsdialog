cbe4c7ab729e1276824798f5c2d5cc6223663954
André Roaldseth <andre.roaldseth@nav.no>
2022-11-17 14:44:44 +0100

    Lag endepunkt for session token i dev

---
+++ src/pages/api/internal/token.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "../../../auth.utils";
+
+async function tokenHandler(req: NextApiRequest, res: NextApiResponse<string>) {
+  const session = await getSession(req);
+  const isDev = process.env.NAIS_CLUSTER_NAME?.startsWith("dev-");
+  if (!session || !isDev) return res.status(401).end();
+  res.status(200).send(session.token);
+}
+
+export default tokenHandler;
