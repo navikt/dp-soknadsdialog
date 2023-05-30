5b34f0b275855c993026b1de89fa2fad9afbf856
Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
2023-05-30 16:00:07 +0200

    Legger til getServerSideProps på generell innsending sin kvittering
    I dokumentasjonen https://nextjs.org/docs/pages/api-reference/functions/get-initial-props sier de at:
    "If getInitialProps is used in a custom _app.js, and the page being navigated to is using getServerSideProps, then getInitialProps will also run on the server."
    
    Vi ser feil på kvitteringssiden på generell innsending, der vi ikke får hentet ut sanity-tekstene på nytt. Det er ikke så rart, siden vi ikke tillater det client side. Legger derfor på en (noe nytteløs) getServerSideProps, for å unngå at vi henter getInitialProps client side.

--- src/pages/generell-innsending/[uuid]/kvittering.tsx
+++ src/pages/generell-innsending/[uuid]/kvittering.tsx
@@ -1,5 +1,88 @@
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next/types";
 import { GenerellInnsendingKvittering } from "../../../components/generell-innsending-kvittering/GenerellInnsendingKvittering";
+import { IDokumentkravList } from "../../../types/documentation.types";
+import { IQuizState } from "../../../types/quiz.types";
+import ErrorPage from "../../_error";
+import { mockGenerellInnsending } from "../../../localhost-data/mock-generell-innsending";
+import { getSession } from "../../../auth.utils";
+import { audienceDPSoknad } from "../../../api.utils";
+import { getSoknadState } from "../../../api/quiz-api";
+import { getDokumentkrav } from "../../api/documentation/[uuid]";
 
-export default function GenerellInnsendingKvitteringPage() {
+interface IProps {
+  soknadState: IQuizState | null;
+  errorCode: number | null;
+  dokumentkravList: IDokumentkravList | null;
+}
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+): Promise<GetServerSidePropsResult<IProps>> {
+  const { query, locale } = context;
+  const uuid = query.uuid as string;
+
+  if (process.env.NEXT_PUBLIC_LOCALHOST) {
+    return {
+      props: {
+        soknadState: mockGenerellInnsending as IQuizState,
+        errorCode: null,
+        dokumentkravList: null,
+      },
+    };
+  }
+
+  const session = await getSession(context.req);
+  if (!session) {
+    return {
+      redirect: {
+        destination: locale ? `/oauth2/login?locale=${locale}` : "/oauth2/login",
+        permanent: false,
+      },
+    };
+  }
+
+  let errorCode = null;
+  let soknadState = null;
+  let dokumentkravList = null;
+
+  const onBehalfOfToken = await session.apiToken(audienceDPSoknad);
+  const soknadStateResponse = await getSoknadState(uuid, onBehalfOfToken);
+
+  if (!soknadStateResponse.ok) {
+    errorCode = soknadStateResponse.status;
+  } else {
+    soknadState = await soknadStateResponse.json();
+  }
+
+  const dokumentkravResponse = await getDokumentkrav(uuid, onBehalfOfToken);
+
+  if (!dokumentkravResponse.ok) {
+    errorCode = dokumentkravResponse.status;
+  } else {
+    dokumentkravList = await dokumentkravResponse.json();
+  }
+
+  return {
+    props: {
+      soknadState,
+      errorCode,
+      dokumentkravList,
+    },
+  };
+}
+
+export default function GenerellInnsendingKvitteringPage(props: IProps) {
+  const { soknadState, dokumentkravList, errorCode } = props;
+
+  if (errorCode || !soknadState || !dokumentkravList) {
+    return (
+      <ErrorPage
+        title="Vi har tekniske problemer akkurat nå"
+        details="Beklager, vi får ikke kontakt med systemene våre. Du kan prøve igjen om litt."
+        statusCode={props.errorCode || 500}
+      />
+    );
+  }
+
   return <GenerellInnsendingKvittering />;
 }
