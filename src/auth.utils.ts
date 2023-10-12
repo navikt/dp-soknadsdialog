ebc028a9a24fb715d9612f2e898b132cd993c2c6
André Roaldseth <andre.roaldseth@nav.no>
2023-10-12 16:32:04 +0200

    Oppgrader til dp-auth 0.3.15 som tracker auth metrics riktig

--- src/auth.utils.ts
+++ src/auth.utils.ts
@@ -1,6 +1,7 @@
 import { GetSessionWithOboProvider, makeSession } from "@navikt/dp-auth";
 import { idporten } from "@navikt/dp-auth/identity-providers";
 import { tokenX, withInMemoryCache } from "@navikt/dp-auth/obo-providers";
+import { withPrometheus } from "@navikt/dp-auth/obo-providers/withPrometheus";
 
 let getSession: GetSessionWithOboProvider;
 
@@ -17,7 +18,7 @@ if (process.env.AUTH_PROVIDER == "local") {
 } else {
   getSession = makeSession({
     identityProvider: idporten,
-    oboProvider: withInMemoryCache(tokenX),
+    oboProvider: withInMemoryCache(withPrometheus(tokenX)),
   });
 }
 
