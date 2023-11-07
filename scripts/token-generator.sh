8aa1c1a60befe8909be5c1e4635b2a0b5cb18094
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-11-07 14:35:19 +0100

    Erstatt single quote med double

--- scripts/token-generator.sh
+++ scripts/token-generator.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-Cyan='\033[0;36m'         # Cyan
-Red='\033[0;31m'          # Red
-Purple='\033[0;35m'       # Purple
-Yellow='\033[0;33m'       # Yellow
-BPurple='\033[1;35m'      # Purple bold
-UGreen='\033[4;32m'       # Green underline
+Cyan="\033[0;36m"         # Cyan
+Red="\033[0;31m"          # Red
+Purple="\033[0;35m"       # Purple
+Yellow="\033[0;33m"       # Yellow
+BPurple="\033[1;35m"      # Purple bold
+UGreen="\033[4;32m"       # Green underline
 
 # Env file 
-envFile='.env.development'
+envFile=".env.development"
 
 # json config 
-jsonConfig='./scripts/token-generator.config.json'
+jsonConfig="./scripts/token-generator.config.json"
 
 # Main script
 init() {
@@ -58,7 +58,7 @@ verifyJQ() {
 # Start token generation process
 startTokenGenerator() {
   # First azure-token-generator url from json config
-  url=$(jq '.' $jsonConfig | jq '.[0].url' | tr -d '"')
+  url=$(jq "." $jsonConfig | jq ".[0].url" | tr -d '"')
   
   # Show link to azureTokenGenerator to user
   echo -e "${Cyan}Visit: ${UGreen}${url}\n"
@@ -69,7 +69,7 @@ startTokenGenerator() {
   read cookie
   echo -e "\n"
 
-  configArray=$(jq -r '.[] | @base64' $jsonConfig)
+  configArray=$(jq -r ".[] | @base64" $jsonConfig)
 
   # Loop through config list and create environment variable
   for config in $configArray;
@@ -78,8 +78,8 @@ startTokenGenerator() {
         echo ${config} | base64 --decode | jq -r ${1}
       }
 
-      env=$(_jq '.env')
-      url=$(_jq '.url')
+      env=$(_jq ".env")
+      url=$(_jq ".url")
 
       generateAndUpdateEnvFile $env $url $cookie
   done
@@ -102,7 +102,7 @@ generateAndUpdateEnvFile() {
     :
   else
     # Add missing env key
-    printf "%s\n" '$a' "${env}" . w | ed -s $envFile
+    printf "%s\n" "$a" "${env}" . w | ed -s $envFile
   fi
 
   # Store access token in variable
@@ -115,7 +115,7 @@ generateAndUpdateEnvFile() {
     generatedEnv="${env}=${accessToken}"
 
     # Update generated env string to env file
-    printf '%s\n' H ,g/^${env}.*/s//${generatedEnv}/ wq | ed -s $envFile
+    printf "%s\n" H ,g/^${env}.*/s//${generatedEnv}/ wq | ed -s $envFile
 
     echo -e "✅ ${Yellow}${env} ${Cyan}updated"
   fi
