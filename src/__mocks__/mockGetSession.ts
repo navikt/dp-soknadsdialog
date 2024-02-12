794e55433453b4353664933a8ba06f353b86faec
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-02-12 07:32:47 +0100

    Oppgradere fra dp-auth til oasis

--- src/__mocks__/mockGetSession.ts
+++ src/__mocks__/mockGetSession.ts
@@ -1,4 +1,4 @@
-import { SessionWithOboProvider } from "@navikt/dp-auth/index/";
+import { SessionWithOboProvider } from "@navikt/oasis/index/";
 
 const mockToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJwaWQiOiIxMjMxMjMxMjMifQ.XdPmoIvLFmgz51eH_05WBNOllgWEtp9kYHkWAHqMwEc";
