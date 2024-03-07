1bba8bc104afdb74b21f0c9980dfed25557b8299
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2024-03-07 12:09:50 +0100

    Ta vekk hook som overskriver brukerens input etter debounce

--- src/components/faktum/faktum-text/FaktumText.tsx
+++ src/components/faktum/faktum-text/FaktumText.tsx
@@ -51,9 +51,9 @@ export function FaktumTextComponent(
     }
   }, [debouncedText]);
 
-  useEffect(() => {
-    setCurrentAnswer(faktum.svar ?? "");
-  }, [faktum]);
+  // useEffect(() => {
+  //   setCurrentAnswer(faktum.svar ?? "");
+  // }, [faktum]);
 
   function onValueChange(event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) {
     const { value } = event.target;
