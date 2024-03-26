eb1ff1ca3b574cf54c321a09403469879141a0c2
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-03-26 16:45:18 +0100

    Refactorerer accordion list

--- src/components/arbeidsforhold/ArbeidsforholdAccordion.tsx
+++ src/components/arbeidsforhold/ArbeidsforholdAccordion.tsx
@@ -1,15 +1,16 @@
 import { PencilIcon, TrashIcon } from "@navikt/aksel-icons";
-import { Accordion, Button } from "@navikt/ds-react";
+import { Accordion, Button, Detail } from "@navikt/ds-react";
 import { IArbeidsforhold, useUserInformation } from "../../context/user-information-context";
 import { useGeneratorUtils } from "../../hooks/useGeneratorUtils";
 import { IQuizGeneratorFaktum, IQuizSeksjon } from "../../types/quiz.types";
 import { FormattedDate } from "../FormattedDate";
 import styles from "./Arbeidsforhold.module.css";
 import { CheckmarkIcon } from "./arbeidsforhold-v2/CheckmarkIcon";
-import { WarningIcon } from "./arbeidsforhold-v2/WarningIcon";
 import { useState } from "react";
 import { getUnansweredFaktumId } from "../faktum/validation/validations.utils";
 import { useValidation } from "../../context/validation-context";
+import { useSanity } from "../../context/sanity-context";
+import { WarningColored } from "@navikt/ds-icons";
 
 interface IProps {
   faktum: IQuizGeneratorFaktum;
@@ -20,6 +21,7 @@ export function ArbeidsforholdAccordion({ faktum, currentSection }: IProps) {
   const [disabled, setDisabled] = useState(false);
   const { addNewGeneratorAnswer } = useGeneratorUtils();
   const { setUnansweredFaktumId } = useValidation();
+  const { getAppText } = useSanity();
   const { arbeidsforhold, setContextSelectedArbeidsforhold, updateContextArbeidsforhold } =
     useUserInformation();
 
@@ -47,38 +49,50 @@ export function ArbeidsforholdAccordion({ faktum, currentSection }: IProps) {
   return (
     <div className={styles.accordion}>
       <Accordion>
-        {arbeidsforhold?.map((arbeidsforhold, index) => (
-          <Accordion.Item key={arbeidsforhold.id} defaultOpen={index === 0}>
-            <Accordion.Header className="arbeidsforhold__accordion">
-              <div>{arbeidsforhold.organisasjonsnavn}</div>
-              <div className={styles.iconContainer}>
-                {arbeidsforhold.utfyllingStatus === "fullført" && <CheckmarkIcon />}
-                {arbeidsforhold.utfyllingStatus === "påbegynt" && <WarningIcon />}
-              </div>
-            </Accordion.Header>
-            <Accordion.Content>
-              <>
-                <FormattedDate date={arbeidsforhold.startdato} /> -{" "}
-                {arbeidsforhold.sluttdato && <FormattedDate date={arbeidsforhold.sluttdato} />}
-              </>
-              <div className={styles.buttonContainer}>
-                <Button
-                  icon={<PencilIcon fontSize="1.5rem" />}
-                  onClick={() => addArbeidsforhold(arbeidsforhold)}
-                  disabled={disabled && arbeidsforhold.utfyllingStatus !== "påbegynt"}
-                >
-                  Fyll ut
-                </Button>
-                <Button
-                  variant="secondary"
-                  icon={<TrashIcon title="a11y-title" fontSize="1.5rem" />}
-                >
-                  Fjern
-                </Button>
-              </div>
-            </Accordion.Content>
-          </Accordion.Item>
-        ))}
+        {arbeidsforhold?.map((arbeidsforhold, index) => {
+          const { sluttdato, startdato, id, utfyllingStatus, organisasjonsnavn } = arbeidsforhold;
+          const fullfort = utfyllingStatus === "fullført";
+          const paabegynt = utfyllingStatus === "påbegynt";
+
+          return (
+            <Accordion.Item key={id} defaultOpen={index === 0}>
+              <Accordion.Header className="arbeidsforhold__accordion">
+                <div>{organisasjonsnavn}</div>
+                <div className={styles.iconContainer}>
+                  {fullfort && <CheckmarkIcon />}
+                  {paabegynt && <WarningColored />}
+                </div>
+              </Accordion.Header>
+              <Accordion.Content>
+                <>
+                  <FormattedDate date={startdato} /> -{" "}
+                  {sluttdato && <FormattedDate date={sluttdato} />}
+                  {paabegynt && (
+                    <Detail uppercase>
+                      <WarningColored />
+                      {getAppText("generator-faktum-kort.delvis-utfylt.varsel")}
+                    </Detail>
+                  )}
+                </>
+                <div className={styles.buttonContainer}>
+                  <Button
+                    icon={<PencilIcon fontSize="1.5rem" />}
+                    onClick={() => addArbeidsforhold(arbeidsforhold)}
+                    disabled={disabled && paabegynt}
+                  >
+                    Fyll ut
+                  </Button>
+                  <Button
+                    variant="secondary"
+                    icon={<TrashIcon title="a11y-title" fontSize="1.5rem" />}
+                  >
+                    Fjern
+                  </Button>
+                </div>
+              </Accordion.Content>
+            </Accordion.Item>
+          );
+        })}
       </Accordion>
     </div>
   );
