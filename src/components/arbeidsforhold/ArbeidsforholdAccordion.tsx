6b167b9913a06ed4133214adee30a10543735226
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-03-26 14:10:42 +0100

    Preutfylle arbeidsted navn når bruker trykke på fyll ut

--- src/components/arbeidsforhold/ArbeidsforholdAccordion.tsx
+++ src/components/arbeidsforhold/ArbeidsforholdAccordion.tsx
@@ -1,23 +1,36 @@
 import { PencilIcon, TrashIcon } from "@navikt/aksel-icons";
 import { Accordion, Button } from "@navikt/ds-react";
-import { IArbeidsforhold } from "../../context/user-information-context";
+import { IArbeidsforhold, useUserInformation } from "../../context/user-information-context";
+import { useGeneratorUtils } from "../../hooks/useGeneratorUtils";
+import { IQuizGeneratorFaktum } from "../../types/quiz.types";
 import { FormattedDate } from "../FormattedDate";
 import styles from "./Arbeidsforhold.module.css";
 import { CheckmarkIcon } from "./arbeidsforhold-v2/CheckmarkIcon";
 
 interface IProps {
+  faktum: IQuizGeneratorFaktum;
   arbeidsforhold: IArbeidsforhold[];
 }
 
-export function ArbeidsforholdAccordion({ arbeidsforhold }: IProps) {
+export function ArbeidsforholdAccordion({ arbeidsforhold, faktum }: IProps) {
+  const { addNewGeneratorAnswer } = useGeneratorUtils();
+  const { setContextSelectedArbeidsforhold } = useUserInformation();
+
+  function addArbeidsforhold(arbeidsforhold: IArbeidsforhold) {
+    addNewGeneratorAnswer(faktum);
+    setContextSelectedArbeidsforhold(arbeidsforhold);
+  }
+
+  // Dette er status til et arbeidsforhold, om det er ferdig eller delvis utført.
+  // Her bør vi bygge et struktur som lever i context
   const status = true;
   return (
     <div className={styles.accordion}>
       <Accordion>
-        {arbeidsforhold?.map(({ id, organisasjonsnavn, startdato, sluttdato }, index) => (
-          <Accordion.Item key={id} defaultOpen={index === 0}>
+        {arbeidsforhold?.map((arbeidsforhold, index) => (
+          <Accordion.Item key={arbeidsforhold.id} defaultOpen={index === 0}>
             <Accordion.Header className="arbeidsforhold__accordion">
-              <div>{organisasjonsnavn}</div>
+              <div>{arbeidsforhold.organisasjonsnavn}</div>
               <div
                 className={styles.iconContainer}
                 aria-label={status ? "Ferdig utfylt" : "Delvis utfylt"}
@@ -27,11 +40,16 @@ export function ArbeidsforholdAccordion({ arbeidsforhold }: IProps) {
             </Accordion.Header>
             <Accordion.Content>
               <>
-                <FormattedDate date={startdato} /> -{" "}
-                {sluttdato && <FormattedDate date={sluttdato} />}
+                <FormattedDate date={arbeidsforhold.startdato} /> -{" "}
+                {arbeidsforhold.sluttdato && <FormattedDate date={arbeidsforhold.sluttdato} />}
               </>
               <div className={styles.buttonContainer}>
-                <Button icon={<PencilIcon title="a11y-title" fontSize="1.5rem" />}>Fyll ut</Button>
+                <Button
+                  icon={<PencilIcon title="a11y-title" fontSize="1.5rem" />}
+                  onClick={() => addArbeidsforhold(arbeidsforhold)}
+                >
+                  Fyll ut
+                </Button>
                 <Button
                   variant="secondary"
                   icon={<TrashIcon title="a11y-title" fontSize="1.5rem" />}
--- src/components/arbeidsforhold/arbeidsforhold-v2/ArbeidsforholdFaktumWrapper_V2.tsx
+++ src/components/arbeidsforhold/arbeidsforhold-v2/ArbeidsforholdFaktumWrapper_V2.tsx
@@ -1,4 +1,6 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { useQuiz } from "../../../context/quiz-context";
+import { useUserInformation } from "../../../context/user-information-context";
 import { QuizFaktum } from "../../../types/quiz.types";
 import { Faktum } from "../../faktum/Faktum";
 
@@ -9,13 +11,38 @@ interface IProps {
 
 export function ArbeidsforholdFaktumWrapper_V2(props: IProps) {
   const { fakta, readonly } = props;
+  const { saveFaktumToQuiz, soknadState } = useQuiz();
+  const { contextSelectedArbeidsforhold } = useUserInformation();
+  const [forceUpdate, setForceUpdate] = useState<boolean>(false);
+
+  const arbeidsforholdBedriftsnavn = fakta.find(
+    (faktum) => faktum.beskrivendeId === "faktum.arbeidsforhold.navn-bedrift",
+  );
+
+  useEffect(() => {
+    if (forceUpdate) setForceUpdate(false);
+  }, [soknadState]);
+
+  useEffect(() => {
+    if (
+      arbeidsforholdBedriftsnavn &&
+      !arbeidsforholdBedriftsnavn?.svar &&
+      contextSelectedArbeidsforhold
+    ) {
+      saveFaktumToQuiz(
+        arbeidsforholdBedriftsnavn,
+        contextSelectedArbeidsforhold?.organisasjonsnavn,
+      );
+      setForceUpdate(true);
+    }
+  }, [fakta]);
 
   return (
     <>
       {fakta.map((faktum) => {
         return (
           <Fragment key={faktum.id}>
-            <Faktum faktum={faktum} readonly={readonly} />
+            <Faktum faktum={faktum} readonly={readonly} forceUpdate={forceUpdate} />
           </Fragment>
         );
       })}
