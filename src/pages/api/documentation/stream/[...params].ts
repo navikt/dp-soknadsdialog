75263d22431e95fc4435acc99f39a8fc9810fcd0
André Roaldseth <andre.roaldseth@nav.no>
2024-10-25 12:12:13 +0200

    Test proxy med next-api-proxy

--- src/pages/api/documentation/stream/[...params].ts
+++ src/pages/api/documentation/stream/[...params].ts
@@ -5,6 +5,7 @@ import { getErrorMessage } from "../../../../utils/api.utils";
 import { getMellomlagringOnBehalfOfToken } from "../../../../utils/auth.utils";
 import { logRequestError } from "../../../../error.logger";
 import { logger } from "@navikt/next-logger";
+import { proxyApiRouteRequest } from "@navikt/next-api-proxy";
 
 const filePath = path.resolve("src/localhost-data/sample.pdf");
 const imageBuffer = fs.readFileSync(filePath);
@@ -12,6 +13,8 @@ const imageBuffer = fs.readFileSync(filePath);
 export const config = {
   api: {
     responseLimit: false,
+    bodyParser: false,
+    externalResolver: true,
   },
 };
 
@@ -34,44 +37,20 @@ async function downloadHandler(req: NextApiRequest, res: NextApiResponse) {
 
     logger.info("Starter streaming av dokumentkrav fil", { urn });
 
-    const response = await fetch(`${process.env.MELLOMLAGRING_BASE_URL}/vedlegg/${urn}`, {
-      headers: {
-        Authorization: `Bearer ${onBehalfOf.token}`,
-      },
+    // Proxy the request
+    await proxyApiRouteRequest({
+      req,
+      res,
+      hostname: "dp-mellomlagring",
+      path: `/v1/obo/mellomlagring/vedlegg/${urn}`,
+      bearerToken: onBehalfOf.token,
+      // use https: false if you are going through service discovery
+      https: false,
     });
-
-    if (!response.ok) {
-      logRequestError(
-        response.statusText,
-        undefined,
-        "Download dokumentkrav files - Failed to download files from dp-mellomlagring",
-      );
-      return res.status(response.status).send(response.statusText);
-    }
-
-    const mellomlagringContentType = response.headers.get("Content-Type");
-
-    // Stream the response directly to the client
-    res.setHeader("Content-Type", mellomlagringContentType || "application/octet-stream");
-    res.setHeader("Content-Disposition", "inline");
-
-    const reader = response.body!.getReader();
-    for await (const chunk of streamAsyncIterable(reader)) {
-      res.write(chunk);
-    }
-    res.end();
   } catch (error) {
     const message = getErrorMessage(error);
     logRequestError(message, undefined, "Download dokumentkrav files - Generic error");
     return res.status(500).send(message);
-  }
-}
-
-async function* streamAsyncIterable(reader: ReadableStreamDefaultReader<Uint8Array>) {
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) break;
-    yield value;
   }
 }
 
