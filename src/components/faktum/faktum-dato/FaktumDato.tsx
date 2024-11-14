863c45c05b71acb70a6c61e8905cf042ae13844a
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-11-14 15:41:50 +0100

    Legge til støtte for single digit date and month

--- src/components/faktum/faktum-dato/FaktumDato.tsx
+++ src/components/faktum/faktum-dato/FaktumDato.tsx
@@ -87,7 +87,8 @@ function FaktumDatoComponent(
   // Trigger validation on blur when user types date manually
   function validateOnBlur() {
     // Pattern for date format dd.mm.yyyy, dd.mm.yy, ddmmyy, ddmmyyyy
-    const dateRegex = /^(?:(0[1-9]|[12][0-9]|3[01])(\.?)(0[1-9]|1[0-2])\2(\d{2}|\d{4}))$/;
+    // Also allows single didgit day and month
+    const dateRegex = /^(?:(0?[1-9]|[12][0-9]|3[01])(\.?)(0?[1-9]|1[0-2])\2(\d{1,2}|\d{4}))$/;
 
     if (inputProps.value && !dateRegex.test(inputProps.value as string)) {
       setErrorMessage(getAppText("validering.ugyldig-dato"));
