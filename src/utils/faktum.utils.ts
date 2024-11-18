624678137f205d6d5f5c259724e8bf8e11f015ce
John Martin Lindseth <john.martin.lindseth@nav.no>
2024-11-18 13:46:57 +0100

    Vis alert hvis man velger et tredjeland i "hvor bor du"
    Filtrer bort gruppeId for tredjeland fra listen i faktumet, i "getLandGruppe", slik at når man sender inn et tredjeland-svar, finner man ingen matchende gruppe. Det fører til at vi får en gruppeId som har en feilmelding definert i Sanity.
    
    Co-authored-by: Nattaphong Klinjan <nattaphong.klinjan@nav.no>
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- src/utils/faktum.utils.ts
+++ src/utils/faktum.utils.ts
@@ -8,6 +8,13 @@ export function findEmployerName(fakta: QuizFaktum[]) {
 
 export function getLandGruppeId(faktum: IQuizLandFaktum, code: string) {
   const outsideLandGruppeId = `${faktum.beskrivendeId}.gruppe.utenfor-landgruppe`;
-  const currentLandGruppeId = faktum.grupper.find((group) => group.land.includes(code))?.gruppeId;
+
+  // Fjern tredjeland-landgruppen fra listen når vi er i "hvilket land bor du i"-spørsmål
+  // Hvis man svarer et land fra den gruppen, ønsker vi å vise "utenfor-landgruppe"-teksten
+  const tredjelandGruppeId = "faktum.hvilket-land-bor-du-i.gruppe.tredjeland"
+  const landgrupperUtenTredjeland = faktum.grupper.filter((group) => group.gruppeId !== tredjelandGruppeId);
+
+  const currentLandGruppeId = landgrupperUtenTredjeland.find((group) => group.land.includes(code))?.gruppeId;
+
   return currentLandGruppeId || outsideLandGruppeId;
 }
