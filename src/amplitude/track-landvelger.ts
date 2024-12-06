bfee3c7ee9fc37e812fe501de96f08cc83e1498a
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-12-06 13:59:38 +0100

    Oppdatere amplitude bostedsland tracking teksten

--- src/amplitude/track-landvelger.ts
+++ src/amplitude/track-landvelger.ts
@@ -1,9 +1,9 @@
 import { track } from "@amplitude/analytics-browser";
 
 export function trackValgtEtLandFraOfteValgteLandListe(skjemanavn: string, land: string) {
-  track(`Valgt et fra ofte valgte land listen`, { skjemanavn, land });
+  track(`Valgt et land fra ofte valgte land listen`, { skjemanavn, land });
 }
 
 export function trackValgtEtLandFraFlereLandListe(skjemanavn: string, land: string) {
-  track(`Valgt et fra flere land listen`, { skjemanavn, land });
+  track(`Valgt et land fra flere land listen`, { skjemanavn, land });
 }
